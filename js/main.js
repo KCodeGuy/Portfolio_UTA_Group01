ba6d7f0162d681838b765ab173a69cedfa3f12b5
tuandm1905 <tuandm1905@gmail.com>
2023-06-20 16:31:38 +0700

    update text-center and reviews swiper

--- js/main.js
+++ js/main.js
@@ -1,92 +1,94 @@
 // KhoaTD handle slider Project SectionĐôi chút về dự án của nhóm
 
-const projectContents = document.querySelectorAll('.project-content');
-const btnProjects = document.querySelectorAll('.btn-project');
+const projectContents = document.querySelectorAll(".project-content");
+const btnProjects = document.querySelectorAll(".btn-project");
 
 btnProjects.forEach((item, index) => {
-    item.addEventListener('click', () => {
-        const projectContent = projectContents[index];
-        const currentActiveBtnProject = document.querySelector('.btn-project.active');
-        const currentActiveContentProject = document.querySelector('.project-content.active');
+  item.addEventListener("click", () => {
+    const projectContent = projectContents[index];
+    const currentActiveBtnProject = document.querySelector(
+      ".btn-project.active"
+    );
+    const currentActiveContentProject = document.querySelector(
+      ".project-content.active"
+    );
 
+    currentActiveContentProject.classList.remove("active");
+    currentActiveBtnProject.classList.remove("active");
 
-        currentActiveContentProject.classList.remove('active');
-        currentActiveBtnProject.classList.remove('active');
-
-        projectContent.classList.add('active');
-        item.classList.add('active');
-    })
-})
+    projectContent.classList.add("active");
+    item.classList.add("active");
+  });
+});
 
 //IT members
 $(document).ready(function () {
-    $("#members-it").owlCarousel({
-        loop: true,
-        margin: 20,
-        autoplay: true,
-        dots: true,
-        autoplayTimeout: 4000,
-        autoplayHoverPause: true,
-        autoplaySpeed: 3000,
-        responsive: {
-            0: {
-                items: 1
-            },
-            2000: {
-                items: 1
-            },
-        },
-    });
+  $("#members-it").owlCarousel({
+    loop: true,
+    margin: 20,
+    autoplay: true,
+    dots: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    autoplaySpeed: 3000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      2000: {
+        items: 1,
+      },
+    },
+  });
 });
 
 //Commercial members
 $(document).ready(function () {
-    $("#members-c").owlCarousel({
-        loop: true,
-        margin: 20,
-        autoplay: true,
-        dots: true,
-        autoplayTimeout: 4000,
-        autoplayHoverPause: true,
-        autoplaySpeed: 3000,
-        responsive: {
-            0: {
-                items: 1
-            },
-            2000: {
-                items: 1
-            },
-        },
-    });
+  $("#members-c").owlCarousel({
+    loop: true,
+    margin: 20,
+    autoplay: true,
+    dots: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    autoplaySpeed: 3000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      2000: {
+        items: 1,
+      },
+    },
+  });
 });
 $(document).ready(function () {
-    $('.slideshow img:first-child').addClass('active').show();
+  $(".slideshow img:first-child").addClass("active").show();
 
-    setInterval(function () {
-        var active = $('.slideshow img.active');
-        var next = active.next('img');
+  setInterval(function () {
+    var active = $(".slideshow img.active");
+    var next = active.next("img");
 
-        if (next.length === 0) {
-            next = $('.slideshow img:first-child');
-        }
+    if (next.length === 0) {
+      next = $(".slideshow img:first-child");
+    }
 
-        active.removeClass('active').fadeOut(0);
-        next.addClass('active').fadeIn(2000);
-    }, 3000); // Thời gian chuyển đổi giữa các hình ảnh (3 giây trong ví dụ)
+    active.removeClass("active").fadeOut(0);
+    next.addClass("active").fadeIn(2000);
+  }, 3000); // Thời gian chuyển đổi giữa các hình ảnh (3 giây trong ví dụ)
 });
 $(document).ready(function () {
-    $('.slideshow1 img:first-child').addClass('active').show();
+  $(".slideshow1 img:first-child").addClass("active").show();
 
-    setInterval(function () {
-        var active = $('.slideshow1 img.active');
-        var next = active.next('img');
+  setInterval(function () {
+    var active = $(".slideshow1 img.active");
+    var next = active.next("img");
 
-        if (next.length === 0) {
-            next = $('.slideshow1 img:first-child');
-        }
+    if (next.length === 0) {
+      next = $(".slideshow1 img:first-child");
+    }
 
-        active.removeClass('active').fadeOut(0);
-        next.addClass('active').fadeIn(2000);
-    }, 3000); // Thời gian chuyển đổi giữa các hình ảnh (3 giây trong ví dụ)
+    active.removeClass("active").fadeOut(0);
+    next.addClass("active").fadeIn(2000);
+  }, 3000); // Thời gian chuyển đổi giữa các hình ảnh (3 giây trong ví dụ)
 });
-
